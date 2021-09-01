63da8502c80c4b84fe916cf4ed1019e7966b8acc
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-09-01 13:21:01 +0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Beka on 01/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
