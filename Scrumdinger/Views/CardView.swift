3f53b35a06e8e62d870462492766f295b2206612
Begzod Bakhriddinov <bekadeveloper@icloud.com>
2021-09-01 21:22:33 +0500

    create card view, add color extension

---
+++ Scrumdinger/Views/CardView.swift
@@ -0,0 +1,38 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Beka on 01/09/21.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3.fill")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock.fill")
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.color.accessibleFontColor)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static let scrum = DailyScrum.data[0]
+    
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.color)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
